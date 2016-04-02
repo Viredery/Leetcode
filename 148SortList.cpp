da38adcc6b3ceb80933042ccd9f97c57e6b88b2a
Viredery <dyrviredery@hotmail.com>
2016-04-02 17:02:46 +0800

    Accepted

---
+++ 148SortList.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if(head == NULL || head->next == NULL)
+            return head;
+        ListNode* formerHead = head;
+        ListNode* latterHead = getMiddleNode(head);
+        formerHead = sortList(formerHead);
+        latterHead = sortList(latterHead);
+        return Merge(formerHead, latterHead);
+    }
+    ListNode* getMiddleNode(ListNode* head) {
+        ListNode *fast, *slow, *prev;
+        prev = head;
+        fast = slow = head->next;
+        while(true) {
+            if(fast != NULL && fast->next != NULL)
+                fast = fast->next->next;
+            else
+                break;
+            prev = slow;
+            slow = slow->next;
+        }
+        prev->next = NULL;
+        return slow;
+    }
+    ListNode* Merge(ListNode* formerHead, ListNode* latterHead) {
+        ListNode result(0);
+        ListNode* current_p = &result;
+        while(true) {
+            if(formerHead != NULL && latterHead != NULL) {
+                if(formerHead->val < latterHead->val) {
+                    current_p->next = formerHead;
+                    current_p = formerHead;
+                    formerHead = formerHead->next;
+                } else {
+                    current_p->next = latterHead;
+                    current_p = latterHead;
+                    latterHead = latterHead->next;
+                }
+            } else {
+                if(formerHead == NULL)
+                    current_p->next = latterHead;
+                else
+                    current_p->next = formerHead;
+                break;
+            }
+        }
+        return result.next;
+    }
+};
