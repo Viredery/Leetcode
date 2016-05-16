1e9d928109dc6b4c0ffa3dd1a175eee65f6bbb88
Viredery <dyrviredery@hotmail.com>
2016-05-16 13:21:29 +0800

    Accepted

--- 58LengthofLastWord.java
+++ 58LengthofLastWord.java
@@ -1,5 +1,6 @@
 public class Solution {
     public int lengthOfLastWord(String s) {
+        s = s.trim();
         int pos = s.lastIndexOf(" ");
 	String lastWord = new String();
         lastWord = s.substring(pos + 1);
