369f1149ea51b65fd81f46adee6fc86c3f993d7d
Viredery <dyrviredery@hotmail.com>
2016-05-30 18:27:07 +0800

    Accepted

--- 226InvertBinaryTree.java
+++ 226InvertBinaryTree.java
@@ -11,21 +11,13 @@ public class Solution {
     public TreeNode invertTree(TreeNode root) {
         if (root == null)
             return root;
-        if (root.left != null && root.right != null) {
-            TreeNode tmp = root.left;
-            root.left = root.right;
-            root.right = tmp;
-            invertTree(root.left);
-            invertTree(root.right);
-        } else if (root.left != null) {
-            root.right = root.left;
-            root.left = null;
-            invertTree(root.right);
-        } else if (root.right != null) {
-            root.left = root.right;
-            root.right = null;
-            invertTree(root.left);
-        }
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+
         return root;
     }
 }
