50fa5ab344c6e25ed269efa5c15d786489d3c9d0
Viredery <dyrviredery@hotmail.com>
2016-06-01 12:01:05 +0800

    Accepted

--- 32LongestValidParentheses.java
+++ 32LongestValidParentheses.java
@@ -3,21 +3,19 @@ public class Solution {
         int[] validParentheses = new int[s.length()];
         int longestValidParentheses = 0;
         for (int i = 0; i != s.length(); i++) {
-        	if (s.charAt(i) == '(')
-        		validParentheses[i] = 0;
-        	else if (i - 1 >= 0 && validParentheses[i - 1] != 0) {
-        		int pos = i - validParentheses[i - 1] - 1;
-        		if (pos >= 0 && s.charAt(pos) == '(') {
-        		    validParentheses[i] = pos - 1 >= 0 ? 
-        		            validParentheses[i - 1] + 2 + validParentheses[pos - 1] : 
-        		            validParentheses[i - 1] + 2;
-        		} else
-        			validParentheses[i] = 0;
-        	} else if (i - 1 >= 0 && s.charAt(i - 1) == '(') {
-        	        validParentheses[i] = i - 2 >= 0 ? validParentheses[i - 2] + 2 : 2;
-        	} else
-        	    validParentheses[i] = 0;
-        	longestValidParentheses = Math.max(validParentheses[i], longestValidParentheses);
+            if (s.charAt(i) == '(')
+                validParentheses[i] = 0;
+            else if (i - 1 >= 0 && validParentheses[i - 1] != 0) {
+                int pos = i - validParentheses[i - 1] - 1;
+                if (pos >= 0 && s.charAt(pos) == '(')
+                    validParentheses[i] = validParentheses[i - 1] + 2 + (pos - 1 >= 0 ? validParentheses[pos - 1] : 0);
+                else
+                    validParentheses[i] = 0;
+            } else if (i - 1 >= 0 && s.charAt(i - 1) == '(') {
+                    validParentheses[i] = i - 2 >= 0 ? validParentheses[i - 2] + 2 : 2;
+            } else
+                validParentheses[i] = 0;
+            longestValidParentheses = Math.max(validParentheses[i], longestValidParentheses);
         }
         return longestValidParentheses;
     }
