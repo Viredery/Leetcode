e3763df0d4a5c8de431e05f814f9268560da7d06
Viredery <dyrviredery@hotmail.com>
2016-06-01 11:52:07 +0800

    Accepted

---
+++ 32LongestValidParentheses.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int longestValidParentheses(String s) {
+        int[] validParentheses = new int[s.length()];
+        int longestValidParentheses = 0;
+        for (int i = 0; i != s.length(); i++) {
+        	if (s.charAt(i) == '(')
+        		validParentheses[i] = 0;
+        	else if (i - 1 >= 0 && validParentheses[i - 1] != 0) {
+        		int pos = i - validParentheses[i - 1] - 1;
+        		if (pos >= 0 && s.charAt(pos) == '(') {
+        		    validParentheses[i] = pos - 1 >= 0 ? 
+        		            validParentheses[i - 1] + 2 + validParentheses[pos - 1] : 
+        		            validParentheses[i - 1] + 2;
+        		} else
+        			validParentheses[i] = 0;
+        	} else if (i - 1 >= 0 && s.charAt(i - 1) == '(') {
+        	        validParentheses[i] = i - 2 >= 0 ? validParentheses[i - 2] + 2 : 2;
+        	} else
+        	    validParentheses[i] = 0;
+        	longestValidParentheses = Math.max(validParentheses[i], longestValidParentheses);
+        }
+        return longestValidParentheses;
+    }
+}
