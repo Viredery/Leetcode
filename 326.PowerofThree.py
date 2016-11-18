1d38e32093a2f7ec376cd5d7865d50e0c1cc0c6b
Viredery <dyrviredery@hotmail.com>
2016-11-19 00:38:29 +0800

    Accepted

---
+++ 326.PowerofThree.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n < 1: return False
+        return (math.log10(n) / math.log10(3)) % 1 == 0
