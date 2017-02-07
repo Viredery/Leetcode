9aaba2babaac53e7fd8d06d78fa67dbccc076f99
Viredery <dyrviredery@hotmail.com>
2017-02-07 13:16:10 +0800

    Update

--- 33.SearchinRotatedSortedArray.cpp
+++ 33.SearchinRotatedSortedArray.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        if (nums.empty())
-            return -1;
-        int left = 1, right = nums.size() - 1;
-        int median = -1;
-        while (left <= right) {
-            int m = (left + right) / 2;
-            if (nums[m] < nums[0] && nums[m] < nums[m - 1]) {
-                median = m;
-                break;
-            } else if (nums[m] < nums[0] && nums[m] > nums[m - 1])
-                right = m - 1;
-            else
-                left = m + 1;
+        int start = 0, end = nums.size() - 1;
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] > nums[end]) {
+                if (target > nums[mid] || target <= nums[end])
+                    start = mid + 1;
+                else
+                    end = mid;
+            } else {
+                if (target > nums[mid] && target <= nums[end])
+                    start = mid + 1;
+                else
+                    end = mid;
+            }
         }
-        int searchLeft = 0, searchRight = nums.size() - 1;
-        if (target == nums[0])
-            return 0;
-        if (median > -1 && target > nums[0])
-            searchRight = median - 1;
-        if (median > -1 && target < nums[0])
-            searchLeft = median;
-        while (searchLeft <= searchRight) {
-            int searchMedian = (searchLeft + searchRight) / 2;
-            if (nums[searchMedian] == target)
-                return searchMedian;
-            else if (nums[searchMedian] < target)
-                searchLeft = searchMedian + 1;
-            else
-                searchRight = searchMedian - 1;
-        }
-        return -1;
+        if (start == end && nums[start] == target) return start;
+        else return -1;
+        
     }
 };
