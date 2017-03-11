a2106708867f57e16815e6546b278216fd125477
Viredery <dyrviredery@hotmail.com>
2017-03-11 14:15:54 +0800

    Accepted

---
+++ 118.PascalsTriangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        if (0 == numRows)
+            return {};
+        vector<vector<int>> res;
+        res.emplace_back(1,1);
+        for (int i = 2; i <= numRows; i++) {
+            vector<int> line(i, 1);
+            for (int j = 1; j != i - 1; j++)
+                line[j] = res.back()[j] + res.back()[j - 1];
+            res.push_back(line);
+        }
+        return res;
+    }
+};
