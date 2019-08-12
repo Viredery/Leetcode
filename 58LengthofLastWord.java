a7d645a28c0e7bebf730617fa66610caf808667f
Viredery <yiran_ding@outlook.com>
2019-08-13 01:49:01 +0800

    easy string

--- 58LengthofLastWord.java
+++
@@ -1,9 +0,0 @@
-public class Solution {
-    public int lengthOfLastWord(String s) {
-        s = s.trim();
-        int pos = s.lastIndexOf(" ");
-	String lastWord = new String();
-        lastWord = s.substring(pos + 1);
-        return lastWord.length();
-    }
-}
