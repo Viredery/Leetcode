c199a01dfeda79d480a9d269a2a4662aaeb71d77
Viredery <yiran_ding@outlook.com>
2019-10-06 19:23:41 +0800

    Medium Array DP

---
+++ DynamicProgramming/0120.Triangle.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        vector<int> dp(triangle.back());
+        for (int i = triangle.size() - 2; i >= 0; i--)
+            for (int j = 0; j != triangle[i].size(); j++)
+                dp[j] = min(dp[j], dp[j+1]) + triangle[i][j];
+        return dp[0];
+    }
+};
