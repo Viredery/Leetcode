ee090217e1c1b2e1d1b8647d8c2fece10b80ec15
Viredery <yiran_ding@outlook.com>
2019-11-04 00:14:13 +0800

    Medium BinarySearch

--- BinarySearch/0034.SearchforaRange.cpp
+++ BinarySearch/0034.SearchforaRange.cpp
@@ -8,27 +8,23 @@ private:
         int left = 0, right = nums.size() - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] == target && (mid == 0 || nums[mid - 1] < target))
-                return mid;
-            else if (nums[mid] < target)
-                left = mid + 1;
-            else 
+            if (target <= nums[mid])
                 right = mid - 1;
+            else
+                left = mid + 1;
         }
-        return -1;
+        return left < nums.size() && nums[left] == target ? left : -1;
     }
     
     int searchLastTarget(vector<int>& nums, int target) {
         int left = 0, right = nums.size() - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] == target && (mid == nums.size() - 1 || nums[mid + 1] > target))
-                return mid;
-            else if (nums[mid] > target)
+            if (target < nums[mid])
                 right = mid - 1;
-            else 
+            else
                 left = mid + 1;
         }
-        return -1;
+        return right >= 0 && nums[right] == target ? right : -1;
     }
 };
