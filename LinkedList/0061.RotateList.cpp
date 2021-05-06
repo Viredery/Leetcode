256c4cb713c28a4f3931d852ac6206199cbf20b3
Viredery <yiran_ding@outlook.com>
2021-05-06 23:49:00 +0800

    Medium LinkedList

--- LinkedList/0061.RotateList.cpp
+++ LinkedList/0061.RotateList.cpp
@@ -3,30 +3,47 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        ListNode* cur = head;
-        int n = 0;
-        for (auto ptr = head; ptr != nullptr; ptr = ptr->next)
-            n++;
-        if (n == 0 || k % n == 0) 
+        int n = getSize(head);
+        if (n == 0) {
             return head;
-        k = k % n;
-        ListNode* fast = head, *slow = head;
-        while (k--)
-            fast = fast->next;
-        while (fast->next) {
-            fast = fast->next;
-            slow = slow->next;
         }
-        fast->next = head;
-        head = slow->next;
-        slow->next = nullptr;
+        k = k % n;
+        if (k == 0) {
+            return head;
+        }
+        ListNode* node = GetTheKthNode(head, n - k);
+        ListNode* new_head = node->next;
+        node->next = nullptr;
+        node = GetTheKthNode(new_head, k);
+        node->next = head;
+        return new_head;
+    }
+    
+private:
+    int getSize(ListNode* head) {
+        int size = 0;
+        while (head) {
+            size++;
+            head = head->next;
+        }
+        return size;
+    }
+    
+    ListNode* GetTheKthNode(ListNode* head, int k) {
+        int num = 1;
+        while (num < k) {
+            num++;
+            head = head->next;
+        }
         return head;
-        
     }
 };
+
