18c5c1b937644355bf6ae714706e409fb678a4a4
Viredery <yiran_ding@outlook.com>
2021-05-27 23:54:57 +0800

    Hard SlidingWindow

--- SlidingWindow/0076.MinimumWindowSubstring.cpp
+++ SlidingWindow/0076.MinimumWindowSubstring.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if (s.empty() || t.empty())
+        if (t.empty() || s.empty()) {
             return "";
-        unordered_map<char, int> hash;
-        for (char c : t)
-            hash[c]++;
-        int cnt = t.size();
-        
-        string res;
-        for (int l = 0, r = 0; r < s.size(); r++) {
-            if (hash[s[r]] > 0)
-                cnt--;
-            hash[s[r]]--;
-            while (l < s.size() && hash[s[l]] < 0)
-                hash[s[l++]]++;
-            if (cnt == 0 && (res.empty() || res.size() > r - l + 1))
-                res = s.substr(l, r - l + 1);
         }
-        return res;   
+        std::unordered_map<char, int> char_count;
+        for (const auto& c : t) {
+            char_count[c]++;
+        }
+        std::string res;
+        int cnt = t.size();
+        for (int l = 0, r = 0; r != s.size(); ++r) {
+            if (char_count[s[r]] > 0) {
+                cnt--;
+            }
+            char_count[s[r]]--;
+            while (l < s.size() && char_count[s[l]] < 0) {
+                char_count[s[l++]]++;
+            }
+            if (cnt == 0 && (res.empty() || res.size() > r - l + 1)) {
+                res = s.substr(l, r - l + 1);
+            }
+        }
+        return res;
     }
 };
+
