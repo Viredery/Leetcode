7d48719e2512bccb74314c7397d6758b9da2fa28
Viredery <yiran_ding@outlook.com>
2022-03-26 19:53:52 +0800

    Easy BitManipulation

--- 190.ReverseBits.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    # @param n, an integer
-    # @return an integer
-    def reverseBits(self, n):
-        reverseResult = 0
-        for _ in range(32):
-            reverseResult = (reverseResult << 1) | (n & 1)
-            n = n >> 1
-        return reverseResult
