2666eebacaac446f41970c2e5508e77284e06300
Viredery <yiran_ding@outlook.com>
2022-04-17 22:56:32 +0800

    Easy LinkedList

---
+++ LinkedList/203.RemoveLinkedListElements.py
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode dummy_head(0, head);
+        head = &dummy_head;
+        while (head->next) {
+            if (head->next->val == val) {
+                head->next = head->next->next;
+            } else {
+                head = head->next;
+            }
+        }
+        return dummy_head.next;
+    }
+};
