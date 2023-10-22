63a2dd0b8d0e0cecea0d2d2876253d8665f3150b
Viredery <yiran_ding@outlook.com>
2023-10-22 16:18:32 +0800

    HashTable

--- HashTable/0049.GroupAnagrams.cpp
+++ HashTable/0049.GroupAnagrams.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> hash;
-        for (auto str : strs) {
-            string key(str);
-            sort(key.begin(), key.end());
-            hash[key].push_back(str);
+        std::vector<std::vector<std::string>> groups;
+        std::unordered_map<std::string, std::vector<std::string>> dict;
+        for (const auto& str : strs) {
+            dict[sort(str)].push_back(str);
         }
-        vector<vector<string>> res;
-        for (auto item : hash)
-            res.push_back(move(item.second));
-        return res;
+        for (auto& [_, group] : dict) {
+            groups.push_back(std::move(group));
+        }
+        return groups;
+        
+    }
+private:
+    std::string sort(const std::string& str) {
+        constexpr int kLetterSize = 26;
+        static std::array<int, kLetterSize> counts;
+        counts.fill(0);
+        for (const auto& c : str) {
+            counts[c - 'a']++;
+        }
+        std::string sorted;
+        sorted.reserve(str.size());
+        for (int i = 0; i != kLetterSize; ++i) {
+            while (counts[i]-- > 0) {
+                sorted.push_back('a' + i);
+            }
+        }
+        return sorted;
     }
 };
